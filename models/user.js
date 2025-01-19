b77daf7bd46584666ca954f1f12362e1f394f99a
SHARIFAALDOSERI <sharifaaldoseri5@gmail.com>
2025-01-19 18:23:44 +0300

    Test Sharifa

---
+++ models/user.js
@@ -0,0 +1,32 @@
+const pharmasys = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+{
+  name:
+  {
+    type: String,
+    required: true,
+  },
+  email: 
+  {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: 
+  {
+    type: String,
+    required: true,
+  },
+  role: 
+  {
+    type: String,
+    enum: ['customer', 'pharmacist', 'admin'],
+    default: 'customer',
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
