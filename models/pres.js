ca352af2f7e13d56bd939f08b43ce69b78bf9bb9
SHARIFAALDOSERI <sharifaaldoseri5@gmail.com>
2025-01-19 20:17:56 +0300

    pres123

--- models/pres.js
+++ models/pres.js
@@ -1,72 +1,48 @@
-const medicines = [];
+const mongoose = require('mongoose');
 
-const addMedicine = () => {
-    const medicineName = document.getElementById('medicineName').value;
-    const dosage = document.getElementById('dosage').value;
-    const quantity = document.getElementById('quantity').value;
-    const instructions = document.getElementById('instructions').value;
-
-    if (medicineName && dosage && quantity && instructions) 
-    {
-        const medicine = 
-        {
-            name: medicineName,
-            dosage: dosage,
-            quantity: quantity,
-            instructions: instructions
-        };
-        medicines.push(medicine);
-
-        document.getElementById('medicineName').value = '';
-        document.getElementById('dosage').value = '';
-        document.getElementById('quantity').value = '';
-        document.getElementById('instructions').value = '';
-
-        displayMedicines();
-    } else 
-
-    {
-        alert("Please fill out all the fields for the medicine.");
-    }
-};
-
-const displayMedicines = () =>
+const prescriptionSchema = new mongoose.Schema
+(
 {
-    const medicinesList = document.getElementById('medicinesList');
-    medicinesList.innerHTML = '';
+  patient: 
+  {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User', 
+    required: true,
+  },
+  medications: 
+  {
+    type: String, 
+    required: true,
+  },
+  dosage: 
+  {
+    type: String,
+    required: true,
+  },
+  doctor: 
+  {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  issueDate: 
+  {
+    type: Date,
+    default: Date.now,
+  },
+  expirationDate: 
+  {
+    type: Date, 
+  },
+  instructionsForThePation: 
+  {
+    type: String,
+  },
+});
 
-    medicines.forEach((medicine, index) => 
-    {
-        const li = document.createElement('li');
-        li.textContent = `Medicine ${index + 1}: ${medicine.name}, Dosage: 
-        ${medicine.dosage}, Quantity: ${medicine.quantity}, Instructions: ${medicine.instructions}`;
-        medicinesList.appendChild(li);
-    });
-};
+const Prescription = mongoose.model('Prescription', prescriptionSchema);
 
-    const generatePrescription = () => 
-    {
-    const patientName = document.getElementById('patientName').value;
-    const patientAge = document.getElementById('patientAge').value;
-    const doctorName = document.getElementById('doctorName').value;
-    const doctorContact = document.getElementById('doctorContact').value;
+module.exports = Prescription;
 
-    if (!patientName || !patientAge || !doctorName || !doctorContact) {
-        alert("Please fill out all the fields for patient and doctor details.");
-        return;
-    }
 
-    const prescription = 
-    {
-        patientName: patientName,
-        patientAge: patientAge,
-        doctorName: doctorName,
-        doctorContact: doctorContact,
-        medicines: medicines
-    };
-
-    displayPrescription(prescription);
-    medicines.length = 0;
-    displayMedicines();
-};
 
