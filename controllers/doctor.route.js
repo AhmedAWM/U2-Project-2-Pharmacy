b6b35d9c1cfb9cf541cc85675b774e7b23642762
SHARIFAALDOSERI <sharifaaldoseri5@gmail.com>
2025-01-21 19:50:26 +0300

    DoctorRouter

---
+++ controllers/doctor.route.js
@@ -0,0 +1,37 @@
+const router = require("express").Router();
+const Medicine = require("../models/medicine");
+
+router.get("/", async (req, res) => 
+{
+    try 
+    {
+        const medicines = await Medicine.find();
+        res.render("medicine.ejs", { medicines });
+    } 
+    catch (error) 
+    {
+        console.error(error);
+        res.status(500).send("Server Error");
+    }
+});
+
+router.get("/new", (req, res) => 
+{
+    res.render("medicine/new.ejs");
+});
+
+router.get("/:medicineId", async (req, res) => 
+{
+    try 
+    {
+        const foundMedicine = await Medicine.findById(req.params.medicineId);
+        res.render("medicine/show.ejs", { medicine: foundMedicine });
+    } 
+    catch (error) 
+    {
+        console.error(error);
+        res.status(500).send("Error fetching medicine details");
+    }
+});
+
+module.exports = router;
